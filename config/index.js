30d9efcc3fec1490479696ea17a3e553db789dde
Kenneth <70316000+kenneth2728@users.noreply.github.com>
2020-12-09 19:00:55 +0800

    Create index.js

---
+++ config/index.js
@@ -0,0 +1,14 @@
+let config = {
+    // Server
+    server: {
+        port: 3000
+    },
+    // Database
+    database: {
+        uri: 'mongodb://localhost/medtrade'
+    },
+    // Authentication
+    jwtKey: 'WvfmmweOzqy4eK/ueTW44j1diLJq9Rmvrhp4UEVvYD0='
+}
+
+module.exports = config
