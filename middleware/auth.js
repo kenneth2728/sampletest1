d0bdded160b2c7a522fa94d20333fe229b00a3cc
Kenneth <70316000+kenneth2728@users.noreply.github.com>
2020-12-09 19:00:59 +0800

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+/*
+    Middleware for verifying JSON Web Tokens
+*/
+
+const jwt = require('jsonwebtoken')
+const { jwtKey } = require('../config/index')
+
+module.exports = async(req, res, next) => {
+    // check for token
+    const cookieToken = req.cookies.jwt
+
+    if (cookieToken === undefined) next()
+
+    // verify token
+    const payload = jwt.verify(cookieToken, jwtKey)
+
+    req.body.user = payload
+    next()
+}
